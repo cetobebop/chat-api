c51948c62e259c7718b028f276d2b5fad535a1a9
carlos <ctorrensorta@gmail.com>
2024-09-11 17:30:33 -0400

    mas correcionesss

--- src/jobs/requestToMyServer.js
+++ src/jobs/requestToMyServer.js
@@ -1,23 +1,27 @@
 import axios from "axios";
 import { config } from "dotenv";
 
-config()
+config();
 
-const url = process.env.MY_SERVER_URL; 
-
+const url = process.env.MY_SERVER_URL;
 
 async function reloadWebsite() {
   try {
-    const res = await axios.get(url)
-    console.log( res.data, " solicitud realizada al servidor")
+    const res = await axios.get(url);
+    console.log(res.data, " solicitud realizada al servidor");
   } catch (error) {
-    console.log("error en la solicitud de reactivacion de servidor")
-  }  
+    console.log("error en la solicitud de reactivacion de servidor");
+    if (error.response) {
+      console.log(error.response.data);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  }
 }
 
-const reloadWebsiteTime = "*/2 * * * *"
+const reloadWebsiteTime = "*/2 * * * *";
 
-export {
-  reloadWebsite,
-  reloadWebsiteTime
-}
+export { reloadWebsite, reloadWebsiteTime };
